57d7b9240bd1015ed93aa98f30056227faa4e452
Pete Corey <petecorey@gmail.com>
2017-05-31 13:52:20 -0400

    Throttle ingest calls

--- inject-detect.js
+++ inject-detect.js
@@ -3,6 +3,8 @@ import { HTTP } from "meteor/http";
 import { Meteor } from "meteor/meteor";
 import { MongoInternals } from "meteor/mongo";
 
+let queries = [];
+
 // https://github.com/meteor/meteor/blob/devel/packages/mongo/mongo_driver.js#L78-L101
 
 export function anonymize(value, key) {
@@ -24,6 +26,9 @@ export function anonymize(value, key) {
     else if (_.isDate(value)) {
         return "date";
     }
+    else if (value instanceof Meteor.Collection.ObjectID) {
+        return "objectId";
+    }
     else if (_.isArray(value)) {
         switch (_.toLower(key)) {
             case "$and":
@@ -39,21 +44,33 @@ export function anonymize(value, key) {
     }
 }
 
+const post = () => {
+    HTTP.post("http://localhost:4000/api/ingest", {
+        data: {
+            application_token: _.get(Meteor.settings, "inject-detect.secret"),
+            queries: queries.splice(0, queries.length)
+        }
+    }, (err, res) => {
+        if (err && _.get(Meteor.settings, "inject-detect.debug")) {
+            console.error("Inject Detect: Error ingesting queries: ", err);
+        }
+        else if (_.get(Meteor.settings, "inject-detect.debug")) {
+            console.log(`Inject Detect: Ingested queries.`);
+        }
+    });
+}
+
+const throttledPost = _.throttle(Meteor.bindEnvironment(post), 1000*30);
+
 function ingest(collection, query, type) {
     if (collection !== "oplog.rs") {
-        HTTP.post("http://localhost:4000/api/ingest", {
-            data: {
-                application_token: _.get(Meteor.settings, "inject-detect.token"),
-                queries: [
-                    {
-                        collection,
-                        queried_at: new Date(),
-                        query: anonymize(query),
-                        type
-                    }
-                ]
-            }
+        queries.push({
+            collection,
+            queried_at: new Date(),
+            query: anonymize(query),
+            type
         });
+        throttledPost();
     }
 }
 
