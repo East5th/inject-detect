d10668c6d81c2d4beb0dad1677d2ad4ab4d64835
Pete Corey <petecorey@gmail.com>
2017-06-27 16:38:38 -0400

    Updated version

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'east5th:inject-detect',
-  version: '0.0.2',
-  // Brief, one-line summary of the package.
-  summary: 'Meteor package for Inject Detect. Inject Detect detects NoSQL Injection attacks in real time.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'east5th:inject-detect',
+    version: '0.0.3',
+    // Brief, one-line summary of the package.
+    summary: 'Meteor package for Inject Detect. Inject Detect detects NoSQL Injection attacks in real time.',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.7');
-  api.use(['ecmascript', 'http']);
-  api.mainModule('inject-detect.js', 'server');
+    api.versionsFrom('1.4.2.7');
+    api.use(['ecmascript', 'http']);
+    api.mainModule('inject-detect.js', 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('east5th:inject-detect');
-  api.mainModule('inject-detect-tests.js');
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('east5th:inject-detect');
+    api.mainModule('inject-detect-tests.js');
 });
